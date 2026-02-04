dc6ce7e2ff9a64807aca5d7d2fa432e207534d42
manpakphoom-netizen <manpakphoom@gmail.com>
2026-02-04 14:41:03 +0700

    4

--- h/index.php
+++ h/index.php
@@ -5,7 +5,7 @@
 <h1>パク e.php</h1>
 
 <form method="post" action="">
-Username <input type="text" name="auser" autodocus require> <br>
+Username <input type="text" name="auser" autofocus require> <br>
 Password <input type="passord" name="apwd" require><br>
 <button type="submit" name="Submit">LOGIN</button>
 </form>

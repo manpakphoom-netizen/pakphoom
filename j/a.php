a9f25f94864b45b9e72ed554214db47d1df4fb35
manpakphoom-netizen <manppakphoom@gmail.com>
2026-02-18 15:59:29 +0700

    asd

--- j/a.php
+++ j/a.php
@@ -2,14 +2,14 @@
 <html>
 <head>
 <meta charset="utf-8">
-<title>Untitled Document</title>
+<title>ภาคภูมิ 66010914015(แมน)</title>
 </head>
 
 <body>
 
-    <h1>ภาคภูมิ 66010914015 </h1>
+    <h1>ภาคภูมิ 66010914015(แมน) </h1>
     <br>
-    <img src="img/1.png width="80">
+    <img src="img/1.png" width="80">
 
 </body>
 </html>

7c9fd6e752c240ef0ff3314395993a54804e1f31
manpakphoom-netizen <manppakphoom@gmail.com>
2026-02-18 16:48:47 +0700

    a

--- k/a.php
+++ k/a.php
@@ -10,15 +10,13 @@
     <h1>ภาคภูมิ 66010914015(แมน) </h1>
     <br>
 
-    <button type="submit" name="Submit" onclick=<img src='img/1.jpg'width='250'>";
-}">OK</button>
+    <button type="submit" name="Submit" onclick=<img src='img/1.jpg'width='250'>OK</button>
 
 <?php
 
 if(isset($_POST['Submit'])){
-    $id = $_POST['a'];
-    $y = substr($id,0,2);
-    echo "<img src='http://202.28.32.211/picture/student/{$y}/{$id}.jpg'width='250'>";
+  
+    echo "<img src='img/1.jpg'width='250'";
 }
 
 ?>

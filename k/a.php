84cac4213c5e4bd82a2f75f5a1cda4d22be2cbcd
manpakphoom-netizen <manppakphoom@gmail.com>
2026-02-18 16:41:27 +0700

    sa

---
+++ k/a.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>ภาคภูมิ 66010914015(แมน)</title>
+</head>
+
+<body>
+
+    <h1>ภาคภูมิ 66010914015(แมน) </h1>
+    <br>
+    <img src="img/1.png" width="200">
+    <form method ="post" action ="">
+    คำค้นหา <input type="text" name="a" autofocus>
+    <button type="submit" name="Submit">OK </button>
+</Form>
+
+</body>
+</html>

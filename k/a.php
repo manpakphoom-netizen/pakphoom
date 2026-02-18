a9195f3130ec6179e196e36cf6ebc29500fcc125
manpakphoom-netizen <manppakphoom@gmail.com>
2026-02-18 16:45:38 +0700

    a

--- k/a.php
+++ k/a.php
@@ -9,11 +9,18 @@
 
     <h1>ภาคภูมิ 66010914015(แมน) </h1>
     <br>
-    <img src="img/1.png" width="200">
-    <form method ="post" action ="">
-    คำค้นหา <input type="text" name="a" autofocus>
-    <button type="submit" name="Submit">OK </button>
-</Form>
 
+    <button type="submit" name="Submit" onclick=<img src='img/1.jpg'width='250'>";
+}">OK</button>
+
+<?php
+
+if(isset($_POST['Submit'])){
+    $id = $_POST['a'];
+    $y = substr($id,0,2);
+    echo "<img src='http://202.28.32.211/picture/student/{$y}/{$id}.jpg'width='250'>";
+}
+
+?>
 </body>
 </html>

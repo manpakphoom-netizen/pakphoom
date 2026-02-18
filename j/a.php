ea74970f3b3e02b466400e1d85431d6ccb29e964
manpakphoom-netizen <manppakphoom@gmail.com>
2026-02-18 13:52:01 +0700

    w

---
+++ j/a.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Untitled Document</title>
+</head>
+
+<body>
+    <h1>ภาคภูมิ 66010914015 </h1>
+    <img src="img/1.jpg width="80">
+</body>
+</html>

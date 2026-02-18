d0b60786afe436fabe0ed9752f4063ffb8b40b1d
manpakphoom-netizen <manppakphoom@gmail.com>
2026-02-18 17:06:19 +0700

    a

--- k/a.php
+++ k/a.php
@@ -1,10 +1,11 @@
 <h1>Pakphoom  Wannachai</h1>
+<br>
+    <button style="background-color:green; color:white;"
+            onClick="window.open('1.jpg')">
+        Click
+    </button>
 
-    <button style="background-color;green; colorwhite"
-        onClick="window.open(1.jpg)";
-    type="button" name="button">OK✅</button>
-
-    <button style="background-color;orange; colorwhite"
-        onClick="window.open(2.jpg)";
-    type="button" name="button">OK✅</button>
-
+    <button style="background-color:orange; color:white;"
+            onClick="window.open('2.jpg')">
+        Click
+    </button>
